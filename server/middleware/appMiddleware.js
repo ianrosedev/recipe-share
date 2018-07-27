d01134d3b415bd4a10d6395cfd38d506120fa558
Ian Rose <ianrosedev@gmail.com>
2018-07-26 22:15:46 -0400

    Add morgan & update package.json info

--- server/middleware/appMiddleware.js
+++ server/middleware/appMiddleware.js
@@ -1,8 +1,14 @@
 import { json } from 'express';
+import morgan from 'morgan';
 
 const appMiddleware = (app) => {
   // Parse JSON request body
   app.use(json());
+
+  // Log HTTP requests
+  if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+  }
 };
 
 export default appMiddleware;
