b0aa47515f87b54c5c388387aff03a20b833cf54
Ian Rose <ianrosedev@gmail.com>
2018-11-26 16:48:29 -0500

    Updates to suppress warnings

--- server/db/index.js
+++ server/db/index.js
@@ -2,10 +2,15 @@ import mongoose from 'mongoose';
 import config from '../config';
 
 mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', true);
+mongoose.set('useCreateIndex', true);
 
 export default async () => {
   try {
-    const connected = await mongoose.connect(config.db.host);
+    const connected = await mongoose.connect(
+      config.db.host,
+      { useNewUrlParser: true }
+    );
 
     if (connected === mongoose) {
       console.log('🎉  connected to DB at:', config.db.host);
--- server/mail/mail.js
+++ server/mail/mail.js
@@ -4,30 +4,28 @@ import hbs from 'nodemailer-express-handlebars';
 import config from '../config';
 
 const { OAuth2 } = google.auth;
+
 const oauth2Client = new OAuth2(
   config.mail.clientID,
   config.mail.clientSecret,
   'https://developers.google.com/oauthplayground'
 );
+oauth2Client.setCredentials({ refresh_token: config.mail.refreshToken });
 
-oauth2Client.setCredentials({
-  refresh_token: config.mail.refreshToken,
-});
-
-const accessToken = oauth2Client
-  .refreshAccessToken()
-  .then(res => res.credentials.access_token)
-  .catch(err => console.log(err));
+const authHeaders = oauth2Client
+  .getRequestHeaders()
+  .then(headers => headers)
+  .catch(err => new Error(err));
 
 const smtpTransport = nodemailer.createTransport({
   service: 'gmail',
   auth: {
+    authHeaders,
     type: 'OAuth2',
     user: 'ianrosedev@gmail.com',
     clientId: config.mail.clientID,
     clientSecret: config.mail.clientSecret,
     refreshToken: config.mail.refreshToken,
-    accessToken,
   },
 });
 
